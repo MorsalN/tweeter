0fa0461a0fa5b0a9e95440edb41361aeaf898008
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-17 15:20:02 -0800

    Showing Tweets from Multiple Users

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,18 +8,39 @@
 $(document).ready(function() {
   console.log('It is working - client.js');
 
-  const tweetData =   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1636930196982
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const $renderTweets = function(tweetsArray) {
+    for (const tweets of tweetsArray) {
+      const $tweet = $createTweetElement(tweets);
+        console.log('tweet: ', $tweet);
+        $('.tweets-container').append($tweet);
+      }
   }
-  
+
   const $createTweetElement = function(obj) {
     const $tweet = $(`
     <article class="tweet">
@@ -48,11 +69,9 @@ $(document).ready(function() {
     `);
     return $tweet;
   };
-  
-  const $tweet = $createTweetElement(tweetData);
 
-  console.log('tweet: ', $tweet);
-  $('.tweet').append($tweet);
+
+  $renderTweets(data);
 
 });
 
